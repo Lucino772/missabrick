697fa06a420741edcf7a06b00dd24e9f514ff562
Lucino772 <lucapalmi772@gmail.com>
2022-08-29 12:55:48 +0200

    refactor: removed old config files

--- components/index.js
+++
@@ -1,7 +0,0 @@
-import register from "preact-custom-element";
-
-import DownloadSetButton from "./DownloadSetButton";
-import UploadButton from "./UploadButton";
-
-register(DownloadSetButton, 'x-download-set-button', ['placeholder'])
-register(UploadButton, 'x-upload-set-button', ['multiple'])
--- tailwind.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  content: ["./app/**/*.{html,js}", "./components/**/*.{html,js,ts,jsx,tsx}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [
-    require('@tailwindcss/forms')
-  ],
-}
