d6729972e589cf4bcb5155429cf5c142486cdad0
Lucino772 <lucapalmi772@gmail.com>
2022-08-30 08:47:04 +0200

    refactor: moved app/routes.py to app/main/views.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,10 @@
 from flask import Flask
 
-from .routes import app_routes
-
 def create_app():
     app = Flask(__name__)
     app.config['MAX_CONTENT_LENGTH'] = 1 * 1000 * 1000
 
-    app.register_blueprint(app_routes)
+    from app.main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
 
     return app
