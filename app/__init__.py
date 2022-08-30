7a08b3842c17e801d72cb16464fff7501b68f5d5
Lucino772 <lucapalmi772@gmail.com>
2022-08-30 08:40:18 +0200

    refactor: moved app/app.py to app/__init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,1 +1,11 @@
-from .app import create_app
+from flask import Flask
+
+from .routes import app_routes
+
+def create_app():
+    app = Flask(__name__)
+    app.config['MAX_CONTENT_LENGTH'] = 1 * 1000 * 1000
+
+    app.register_blueprint(app_routes)
+
+    return app
