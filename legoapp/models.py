c4971362aa5308730c1bd3049fa7ff37c37420a5
Lucino772 <lucapalmi772@gmail.com>
2022-12-20 00:31:37 +0100

    feat(models): added relationships between models

--- legoapp/models.py
+++ legoapp/models.py
@@ -1,41 +1,40 @@
 from django.db import models
 
+class Theme(models.Model):
+    id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=255)
+
+    parent = models.ForeignKey('Theme', null=True, on_delete=models.DO_NOTHING)
+
 class Color(models.Model):
     id = models.IntegerField(primary_key=True)
     name = models.CharField(max_length=255)
     rgb = models.CharField(max_length=20)
     is_trans = models.BooleanField()
 
+class PartsCategory(models.Model):
+    id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=255)
+
+class Part(models.Model):
+    part_num = models.CharField(primary_key=True, max_length=20)
+    name = models.CharField(max_length=255)
+    part_material = models.CharField(max_length=255)
+
+    part_category = models.ForeignKey(PartsCategory, on_delete=models.DO_NOTHING)
+
+class PartsRelationship(models.Model):
+    id = models.AutoField(primary_key=True)
+    rel_type = models.CharField(max_length=10)
+
+    child_part = models.ForeignKey(Part, on_delete=models.DO_NOTHING, related_name='child')
+    parent_part = models.ForeignKey(Part, on_delete=models.DO_NOTHING, related_name='parent')
+
 class Element(models.Model):
     element_id = models.IntegerField(primary_key=True)
-    part_num = models.CharField(max_length=20)
-    color_id = models.IntegerField()
 
-class Inventory(models.Model):
-    id = models.IntegerField(primary_key=True)
-    version = models.IntegerField()
-    set_num = models.CharField(max_length=20)
-
-class InventoryMinifigs(models.Model):
-    id = models.AutoField(primary_key=True)
-    inventory_id = models.IntegerField()
-    fig_num = models.CharField(max_length=20)
-    quantity = models.IntegerField()
-
-class InventoryParts(models.Model):
-    id = models.AutoField(primary_key=True)
-    inventory_id = models.IntegerField()
-    part_num = models.CharField(max_length=20)
-    color_id = models.IntegerField()
-    quantity = models.IntegerField()
-    is_spare = models.BooleanField()
-    img_url = models.URLField()
-
-class InventorySets(models.Model):
-    id = models.AutoField(primary_key=True)
-    inventory_id = models.IntegerField()
-    set_num = models.CharField(max_length=20)
-    quantity = models.IntegerField()
+    color = models.ForeignKey(Color, on_delete=models.DO_NOTHING)
+    part = models.ForeignKey(Part, on_delete=models.DO_NOTHING, null=True)
 
 class Minifig(models.Model):
     fig_num = models.CharField(primary_key=True, max_length=20)
@@ -43,31 +42,44 @@ class Minifig(models.Model):
     num_parts = models.IntegerField()
     img_url = models.URLField()
 
-class PartsCategory(models.Model):
-    id = models.IntegerField(primary_key=True)
-    name = models.CharField(max_length=255)
-
-class PartsRelationship(models.Model):
-    id = models.AutoField(primary_key=True)
-    rel_type = models.CharField(max_length=10)
-    child_part_num = models.CharField(max_length=20)
-    parent_part_num = models.CharField(max_length=20)
-
-class Part(models.Model):
-    part_num = models.CharField(primary_key=True, max_length=20)
-    name = models.CharField(max_length=255)
-    part_cat_id = models.IntegerField()
-    part_material = models.CharField(max_length=255)
-
 class Set(models.Model):
     set_num = models.CharField(primary_key=True, max_length=20)
     name = models.CharField(max_length=255)
     year = models.IntegerField()
-    theme_id = models.IntegerField()
     num_parts = models.IntegerField()
     img_url = models.URLField()
 
-class Theme(models.Model):
+    theme = models.ForeignKey(Theme, on_delete=models.DO_NOTHING)
+
+class Inventory(models.Model):
     id = models.IntegerField(primary_key=True)
-    name = models.CharField(max_length=255)
-    parent_id = models.IntegerField(null=True)
+    version = models.IntegerField()
+    is_minifig = models.BooleanField()
+
+    _set = models.ForeignKey(Set, on_delete=models.DO_NOTHING, null=True)
+    minifig = models.ForeignKey(Minifig, on_delete=models.DO_NOTHING, null=True)
+
+class InventoryMinifigs(models.Model):
+    id = models.AutoField(primary_key=True)
+    quantity = models.IntegerField()
+
+    inventory = models.ForeignKey(Inventory, on_delete=models.DO_NOTHING, null=True)
+    minifig = models.ForeignKey(Minifig, on_delete=models.DO_NOTHING)
+
+class InventoryParts(models.Model):
+    id = models.AutoField(primary_key=True)
+    quantity = models.IntegerField()
+    is_spare = models.BooleanField()
+    img_url = models.URLField()
+
+    inventory = models.ForeignKey(Inventory, on_delete=models.DO_NOTHING, null=True)
+    part = models.ForeignKey(Part, on_delete=models.DO_NOTHING)
+    color = models.ForeignKey(Color, on_delete=models.DO_NOTHING)
+
+class InventorySets(models.Model):
+    id = models.AutoField(primary_key=True)
+    quantity = models.IntegerField()
+
+    inventory = models.ForeignKey(Inventory, on_delete=models.DO_NOTHING, null=True)
+    _set = models.ForeignKey(Set, on_delete=models.DO_NOTHING, null=True)
+
--- scripts/load_rebrickable.py
+++ scripts/load_rebrickable.py
@@ -7,9 +7,20 @@ import tempfile
 
 import requests
 
-from legoapp.models import (Color, Element, Inventory, InventoryMinifigs,
-                            InventoryParts, InventorySets, Minifig, Part,
-                            PartsCategory, PartsRelationship, Set, Theme)
+from legoapp.models import (
+    Color, 
+    Element, 
+    Inventory, 
+    InventoryMinifigs,
+    InventoryParts, 
+    InventorySets, 
+    Minifig, 
+    Part,
+    PartsCategory,
+    PartsRelationship,
+    Set,
+    Theme
+)
 
 
 def _donwload_gzip_file(url: str, dest: str):
@@ -29,6 +40,30 @@ def _read_csv(filename: str):
 
         yield reader
 
+def _get_default(model, key: str, value, default = None):
+    try:
+        return model.objects.get(**{ key: value })
+    except model.DoesNotExist:
+        return default
+
+def _import_themes(filename: str):
+    with _read_csv(filename) as reader:
+        Theme.objects.all().delete()
+        themes = {
+            int(row[0]): (Theme(
+                id=int(row[0]),
+                name=row[1],
+                parent=None
+            ), row[2])
+            for row in reader
+        }
+
+        for theme, parent_id in themes.values():
+            if parent_id.isnumeric():
+                theme.parent = themes[int(parent_id)][0]
+
+        Theme.objects.bulk_create([theme for theme, parent_id in themes.values()])
+
 def _import_colors(filename: str):
     with _read_csv(filename) as reader:
         Color.objects.all().delete()
@@ -43,73 +78,58 @@ def _import_colors(filename: str):
         ]
         Color.objects.bulk_create(colors)
 
+def _import_part_categories(filename: str):
+    with _read_csv(filename) as reader:
+        PartsCategory.objects.all().delete()
+        categories = [
+            PartsCategory(
+                id=int(row[0]),
+                name=row[1],
+            )
+            for row in reader
+        ]
+        PartsCategory.objects.bulk_create(categories)
+
+def _import_parts(filename: str):
+    with _read_csv(filename) as reader:
+        Part.objects.all().delete()
+        parts = [
+            Part(
+                part_num=row[0],
+                name=row[1],
+                part_category=PartsCategory.objects.get(id=int(row[2])),
+                part_material=row[3],
+            )
+            for row in reader
+        ]
+        Part.objects.bulk_create(parts)
+
+def _import_part_relationships(filename: str):
+    with _read_csv(filename) as reader:
+        PartsRelationship.objects.all().delete()
+        relationships = [
+            PartsRelationship(
+                rel_type=row[0],
+                child_part=Part.objects.get(part_num=row[1]),
+                parent_part=Part.objects.get(part_num=row[2])
+            )
+            for row in reader
+        ]
+        PartsRelationship.objects.bulk_create(relationships)
+
 def _import_elements(filename: str):
     with _read_csv(filename) as reader:
         Element.objects.all().delete()
+
         elements = [
             Element(
                 element_id=int(row[0]),
-                part_num=row[1],
-                color_id=int(row[2])
+                part=_get_default(Part, 'part_num', row[1]),
+                color=Color.objects.get(id=int(row[2]))
             )
             for row in reader
         ]
         Element.objects.bulk_create(elements)
-
-def _import_inventories(filename: str):
-    with _read_csv(filename) as reader:
-        Inventory.objects.all().delete()
-        inventories = [
-            Inventory(
-                id=int(row[0]),
-                version=int(row[1]),
-                set_num=row[2]
-            )
-            for row in reader
-        ]
-        Inventory.objects.bulk_create(inventories)
-
-def _import_inventory_minifigs(filename: str):
-    with _read_csv(filename) as reader:
-        InventoryMinifigs.objects.all().delete()
-        inventories = [
-            InventoryMinifigs(
-                inventory_id=int(row[0]),
-                fig_num=row[1],
-                quantity=int(row[2])
-            )
-            for row in reader
-        ]
-        InventoryMinifigs.objects.bulk_create(inventories)
-
-def _import_inventory_parts(filename: str):
-    with _read_csv(filename) as reader:
-        InventoryParts.objects.all().delete()
-        inventories = [
-            InventoryParts(
-                inventory_id=int(row[0]),
-                part_num=row[1],
-                color_id=int(row[2]),
-                quantity=int(row[3]),
-                is_spare=row[4] == 't',
-                img_url=row[5]
-            )
-            for row in reader
-        ]
-        InventoryParts.objects.bulk_create(inventories)
-
-def _import_inventory_sets(filename: str):
-    with _read_csv(filename) as reader:
-        InventorySets.objects.all().delete()
-        inventories = [
-            InventorySets(
-                inventory_id=int(row[0]),
-                set_num=row[1],
-                quantity=int(row[2])
-            )
-            for row in reader
-        ]
-        InventorySets.objects.bulk_create(inventories)
 
 def _import_minifigs(filename: str):
     with _read_csv(filename) as reader:
@@ -125,45 +145,6 @@ def _import_minifigs(filename: str):
         ]
         Minifig.objects.bulk_create(minifigs)
 
-def _import_part_categories(filename: str):
-    with _read_csv(filename) as reader:
-        PartsCategory.objects.all().delete()
-        categories = [
-            PartsCategory(
-                id=int(row[0]),
-                name=row[1],
-            )
-            for row in reader
-        ]
-        PartsCategory.objects.bulk_create(categories)
-
-def _import_part_relationships(filename: str):
-    with _read_csv(filename) as reader:
-        PartsRelationship.objects.all().delete()
-        relationships = [
-            PartsRelationship(
-                rel_type=row[0],
-                child_part_num=row[1],
-                parent_part_num=row[2]
-            )
-            for row in reader
-        ]
-        PartsRelationship.objects.bulk_create(relationships)
-
-def _import_parts(filename: str):
-    with _read_csv(filename) as reader:
-        Part.objects.all().delete()
-        parts = [
-            Part(
-                part_num=row[0],
-                name=row[1],
-                part_cat_id=int(row[2]),
-                part_material=row[3],
-            )
-            for row in reader
-        ]
-        Part.objects.bulk_create(parts)
-
 def _import_sets(filename: str):
     with _read_csv(filename) as reader:
         Set.objects.all().delete()
@@ -172,7 +153,7 @@ def _import_sets(filename: str):
                 set_num=row[0],
                 name=row[1],
                 year=int(row[2]),
-                theme_id=int(row[3]),
+                theme=Theme.objects.get(id=int(row[3])),
                 num_parts=int(row[4]),
                 img_url=row[5]
             )
@@ -180,18 +161,66 @@ def _import_sets(filename: str):
         ]
         Set.objects.bulk_create(sets)
 
-def _import_themes(filename: str):
+def _import_inventories(filename: str):
     with _read_csv(filename) as reader:
-        Theme.objects.all().delete()
-        themes = [
-            Theme(
+        Inventory.objects.all().delete()
+        inventories = [
+            Inventory(
                 id=int(row[0]),
-                name=row[1],
-                parent_id=int(row[2]) if str(row[2]).isnumeric() else None
+                version=int(row[1]),
+                is_minifig = row[2].startswith('fig-'),
+                _set=Set.objects.get(set_num=row[2]) if not row[2].startswith('fig-') else None,
+                minifig=Minifig.objects.get(fig_num=row[2]) if row[2].startswith('fig-') else None,
             )
             for row in reader
         ]
-        Theme.objects.bulk_create(themes)
+        Inventory.objects.bulk_create(inventories)
+
+def _import_inventory_minifigs(filename: str):
+    with _read_csv(filename) as reader:
+        InventoryMinifigs.objects.all().delete()
+
+        inventories = [
+            InventoryMinifigs(
+                inventory=_get_default(Inventory, 'id', int(row[0])),
+                minifig=Minifig.objects.get(fig_num=row[1]),
+                quantity=int(row[2])
+            )
+            for row in reader
+        ]
+        InventoryMinifigs.objects.bulk_create(inventories)
+
+def _import_inventory_parts(filename: str):
+    with _read_csv(filename) as reader:
+        InventoryParts.objects.all().delete()
+
+        inventories = [
+            InventoryParts(
+                inventory=_get_default(Inventory, 'id', int(row[0])),
+                part=Part.objects.get(part_num=row[1]),
+                color=Color.objects.get(id=int(row[2])),
+                quantity=int(row[3]),
+                is_spare=row[4] == 't',
+                img_url=row[5]
+            )
+            for row in reader
+        ]
+        InventoryParts.objects.bulk_create(inventories)
+
+def _import_inventory_sets(filename: str):
+    with _read_csv(filename) as reader:
+        InventorySets.objects.all().delete()
+
+        inventories = [
+            InventorySets(
+                inventory=_get_default(Inventory, 'id', int(row[0])),
+                _set=_get_default(Set, 'set_num', row[1]),
+                quantity=int(row[2])
+            )
+            for row in reader
+        ]
+
+        InventorySets.objects.bulk_create(inventories)
 
 
 _DATA_URLS = [
@@ -201,12 +230,12 @@ _DATA_URLS = [
     ('https://cdn.rebrickable.com/media/downloads/parts.csv.gz', _import_parts),
     ('https://cdn.rebrickable.com/media/downloads/part_relationships.csv.gz', _import_part_relationships),
     ('https://cdn.rebrickable.com/media/downloads/elements.csv.gz', _import_elements),
-    ('https://cdn.rebrickable.com/media/downloads/sets.csv.gz', _import_sets),
     ('https://cdn.rebrickable.com/media/downloads/minifigs.csv.gz', _import_minifigs),
+    ('https://cdn.rebrickable.com/media/downloads/sets.csv.gz', _import_sets),
     ('https://cdn.rebrickable.com/media/downloads/inventories.csv.gz', _import_inventories),
+    ('https://cdn.rebrickable.com/media/downloads/inventory_minifigs.csv.gz', _import_inventory_minifigs),
     ('https://cdn.rebrickable.com/media/downloads/inventory_parts.csv.gz', _import_inventory_parts),
     ('https://cdn.rebrickable.com/media/downloads/inventory_sets.csv.gz', _import_inventory_sets),
-    ('https://cdn.rebrickable.com/media/downloads/inventory_minifigs.csv.gz', _import_inventory_minifigs)
 ]
 
 
