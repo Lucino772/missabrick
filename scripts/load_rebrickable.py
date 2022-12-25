076e313c3557c2302e20231d59e5113f04aa2974
Lucino772 <lucapalmi772@gmail.com>
2022-12-25 01:55:58 +0100

    perf: improved performance of load_rebrickable script using python generators

--- scripts/load_rebrickable.py
+++ scripts/load_rebrickable.py
@@ -6,6 +6,8 @@ import shutil
 import tempfile
 
 import requests
+from typing import Type, Callable, Iterator, List, Any, Generator, TypeVar
+from django.db import models
 
 from legoapp.models import (
     Color, 
@@ -46,182 +48,157 @@ def _get_default(model, key: str, value, default = None):
     except model.DoesNotExist:
         return default
 
-def _import_themes(filename: str):
-    with _read_csv(filename) as reader:
-        Theme.objects.all().delete()
-        themes = {
-            int(row[0]): (Theme(
+Model_T = TypeVar('Model_T')
+ProcessRow_Return_T = TypeVar('ProcessRow_Return_T')
+
+def _get_importer(
+    _cls: Type[Model_T],
+    process_row: Callable[[Iterator[List[str]]], ProcessRow_Return_T],
+    after_process: Callable[[ProcessRow_Return_T], Generator[Model_T, None, None]] = None
+):
+    def _importer(filename: str):
+        with _read_csv(filename) as reader:
+            _cls.objects.all().delete()
+            _rows = map(process_row, reader)
+            if callable(after_process):
+                _rows = after_process(_rows)
+            
+            _cls.objects.bulk_create(_rows)
+    
+    return _importer
+
+def _themes_after_process(themes):
+    _themes = dict(themes)
+    for theme, parent_id in _themes.values():
+        if parent_id.isnumeric():
+            theme.parent = _themes[int(parent_id)][0]
+        
+        yield theme
+
+_import_themes = _get_importer(
+    Theme, 
+    lambda row: (
+        int(row[0]),
+        (
+            Theme(
                 id=int(row[0]),
                 name=row[1],
                 parent=None
-            ), row[2])
-            for row in reader
-        }
+            ), 
+            row[2]
+        )
+    ),
+    _themes_after_process
+)
 
-        for theme, parent_id in themes.values():
-            if parent_id.isnumeric():
-                theme.parent = themes[int(parent_id)][0]
+_import_colors = _get_importer(
+    Color,
+    lambda row: Color(
+        id=int(row[0]),
+        name=row[1],
+        rgb=row[2],
+        is_trans=(row[3] == 't')
+    )
+)
 
-        Theme.objects.bulk_create([theme for theme, parent_id in themes.values()])
+_import_part_categories = _get_importer(
+    PartsCategory,
+    lambda row: PartsCategory(
+        id=int(row[0]),
+        name=row[1],
+    )
+)
 
-def _import_colors(filename: str):
-    with _read_csv(filename) as reader:
-        Color.objects.all().delete()
-        colors = [
-            Color(
-                id=int(row[0]),
-                name=row[1],
-                rgb=row[2],
-                is_trans=(row[3] == 't')
-            )
-            for row in reader
-        ]
-        Color.objects.bulk_create(colors)
+_import_parts = _get_importer(
+    Part,
+    lambda row: Part(
+        part_num=row[0],
+        name=row[1],
+        part_category=PartsCategory.objects.get(id=int(row[2])),
+        part_material=row[3],
+    )
+)
 
-def _import_part_categories(filename: str):
-    with _read_csv(filename) as reader:
-        PartsCategory.objects.all().delete()
-        categories = [
-            PartsCategory(
-                id=int(row[0]),
-                name=row[1],
-            )
-            for row in reader
-        ]
-        PartsCategory.objects.bulk_create(categories)
+_import_part_relationships = _get_importer(
+    PartsRelationship,
+    lambda row: PartsRelationship(
+        rel_type=row[0],
+        child_part=_get_default(Part, 'part_num', row[1]),
+        parent_part=_get_default(Part, 'part_num', row[2])
+    )
+)
 
-def _import_parts(filename: str):
-    with _read_csv(filename) as reader:
-        Part.objects.all().delete()
-        parts = [
-            Part(
-                part_num=row[0],
-                name=row[1],
-                part_category=PartsCategory.objects.get(id=int(row[2])),
-                part_material=row[3],
-            )
-            for row in reader
-        ]
-        Part.objects.bulk_create(parts)
+_import_elements = _get_importer(
+    Element,
+    lambda row: Element(
+        element_id=int(row[0]),
+        part=_get_default(Part, 'part_num', row[1]),
+        color=Color.objects.get(id=int(row[2]))
+    )
+)
 
-def _import_part_relationships(filename: str):
-    with _read_csv(filename) as reader:
-        PartsRelationship.objects.all().delete()
-        relationships = [
-            PartsRelationship(
-                rel_type=row[0],
-                child_part=_get_default(Part, 'part_num', row[1]),
-                parent_part=_get_default(Part, 'part_num', row[2])
-            )
-            for row in reader
-        ]
-        PartsRelationship.objects.bulk_create(relationships)
+_import_minifigs = _get_importer(
+    Minifig,
+    lambda row: Minifig(
+        fig_num=row[0],
+        name=row[1],
+        num_parts=int(row[2]),
+        img_url=row[3]
+    )
+)
 
-def _import_elements(filename: str):
-    with _read_csv(filename) as reader:
-        Element.objects.all().delete()
+_import_sets = _get_importer(
+    Set,
+    lambda row: Set(
+        set_num=row[0],
+        name=row[1],
+        year=int(row[2]),
+        theme=Theme.objects.get(id=int(row[3])),
+        num_parts=int(row[4]),
+        img_url=row[5]
+    )
+)
 
-        elements = [
-            Element(
-                element_id=int(row[0]),
-                part=_get_default(Part, 'part_num', row[1]),
-                color=Color.objects.get(id=int(row[2]))
-            )
-            for row in reader
-        ]
-        Element.objects.bulk_create(elements)
+_import_inventories = _get_importer(
+    Inventory,
+    lambda row: Inventory(
+        id=int(row[0]),
+        version=int(row[1]),
+        is_minifig = row[2].startswith('fig-'),
+        _set=Set.objects.get(set_num=row[2]) if not row[2].startswith('fig-') else None,
+        minifig=Minifig.objects.get(fig_num=row[2]) if row[2].startswith('fig-') else None,
+    )
+)
 
-def _import_minifigs(filename: str):
-    with _read_csv(filename) as reader:
-        Minifig.objects.all().delete()
-        minifigs = [
-            Minifig(
-                fig_num=row[0],
-                name=row[1],
-                num_parts=int(row[2]),
-                img_url=row[3]
-            )
-            for row in reader
-        ]
-        Minifig.objects.bulk_create(minifigs)
+_import_inventory_minifigs = _get_importer(
+    InventoryMinifigs,
+    lambda row: InventoryMinifigs(
+        inventory=_get_default(Inventory, 'id', int(row[0])),
+        minifig=Minifig.objects.get(fig_num=row[1]),
+        quantity=int(row[2])
+    )
+)
 
-def _import_sets(filename: str):
-    with _read_csv(filename) as reader:
-        Set.objects.all().delete()
-        sets = [
-            Set(
-                set_num=row[0],
-                name=row[1],
-                year=int(row[2]),
-                theme=Theme.objects.get(id=int(row[3])),
-                num_parts=int(row[4]),
-                img_url=row[5]
-            )
-            for row in reader
-        ]
-        Set.objects.bulk_create(sets)
+_import_inventory_parts = _get_importer(
+    InventoryParts,
+    lambda row: InventoryParts(
+        inventory=_get_default(Inventory, 'id', int(row[0])),
+        part=Part.objects.get(part_num=row[1]),
+        color=Color.objects.get(id=int(row[2])),
+        quantity=int(row[3]),
+        is_spare=row[4] == 't',
+        img_url=row[5]
+    )
+)
 
-def _import_inventories(filename: str):
-    with _read_csv(filename) as reader:
-        Inventory.objects.all().delete()
-        inventories = [
-            Inventory(
-                id=int(row[0]),
-                version=int(row[1]),
-                is_minifig = row[2].startswith('fig-'),
-                _set=Set.objects.get(set_num=row[2]) if not row[2].startswith('fig-') else None,
-                minifig=Minifig.objects.get(fig_num=row[2]) if row[2].startswith('fig-') else None,
-            )
-            for row in reader
-        ]
-        Inventory.objects.bulk_create(inventories)
-
-def _import_inventory_minifigs(filename: str):
-    with _read_csv(filename) as reader:
-        InventoryMinifigs.objects.all().delete()
-
-        inventories = [
-            InventoryMinifigs(
-                inventory=_get_default(Inventory, 'id', int(row[0])),
-                minifig=Minifig.objects.get(fig_num=row[1]),
-                quantity=int(row[2])
-            )
-            for row in reader
-        ]
-        InventoryMinifigs.objects.bulk_create(inventories)
-
-def _import_inventory_parts(filename: str):
-    with _read_csv(filename) as reader:
-        InventoryParts.objects.all().delete()
-
-        inventories = [
-            InventoryParts(
-                inventory=_get_default(Inventory, 'id', int(row[0])),
-                part=Part.objects.get(part_num=row[1]),
-                color=Color.objects.get(id=int(row[2])),
-                quantity=int(row[3]),
-                is_spare=row[4] == 't',
-                img_url=row[5]
-            )
-            for row in reader
-        ]
-        InventoryParts.objects.bulk_create(inventories)
-
-def _import_inventory_sets(filename: str):
-    with _read_csv(filename) as reader:
-        InventorySets.objects.all().delete()
-
-        inventories = [
-            InventorySets(
-                inventory=_get_default(Inventory, 'id', int(row[0])),
-                _set=_get_default(Set, 'set_num', row[1]),
-                quantity=int(row[2])
-            )
-            for row in reader
-        ]
-
-        InventorySets.objects.bulk_create(inventories)
-
+_import_inventory_sets = _get_importer(
+    InventorySets,
+    lambda row: InventorySets(
+        inventory=_get_default(Inventory, 'id', int(row[0])),
+        _set=_get_default(Set, 'set_num', row[1]),
+        quantity=int(row[2])
+    )
+)
 
 _DATA_URLS = [
     ('https://cdn.rebrickable.com/media/downloads/themes.csv.gz', _import_themes),
