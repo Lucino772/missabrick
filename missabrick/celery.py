ab1d2fb27a156864a4f547df7886c9705e92ffc4
Lucino772 <lucapalmi772@gmail.com>
2023-01-26 22:30:49 +0100

    feat: added redis config for celery

--- missabrick/celery.py
+++ missabrick/celery.py
@@ -6,4 +6,5 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'missabrick.settings')
 
 celery = Celery('missabrick')
 celery.config_from_object('django.conf:settings', namespace='CELERY')
+celery.conf.broker_url = os.getenv('MISSABRICK_CELERY_BROKER_URL')
 celery.autodiscover_tasks()
