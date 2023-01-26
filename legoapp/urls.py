2b9c2fb3d13fe08f99e743fb62aaeb6e2f5ac18a
Lucino772 <lucapalmi772@gmail.com>
2023-01-26 16:23:10 +0100

    feat: added /explore view using tabler + removed tailwindcss

--- legoapp/urls.py
+++ legoapp/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('explore', views.explore, name='explore'),
     path('download/<str:set_number>', views.download_set, name='download'),
     path('report', views.report, name='report')
 ]
