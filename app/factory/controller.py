88359d542393085cb00a46f8a6f3d8ab96d73f79
Lucino772 <lucapalmi772@gmail.com>
2023-06-20 00:44:06 +0200

    refactor(mvc): added controller factory

---
+++ app/factory/controller.py
@@ -0,0 +1,19 @@
+from app.controllers.explore import ExploreController
+from app.controllers.login import LoginController
+from app.controllers.report import ReportController
+from app.interfaces.factory.controller import IControllerFactory
+from app.interfaces.views.explore import IExploreView
+from app.interfaces.views.login import ILoginView
+from app.interfaces.views.report import IReportView
+
+
+class ControllerFactory(IControllerFactory):
+    def get_controller(self, view):
+        if isinstance(IExploreView):
+            return ExploreController(view)
+        elif isinstance(ILoginView):
+            return LoginController(view)
+        elif isinstance(IReportView):
+            return ReportController(view)
+        else:
+            raise RuntimeError("Invalid view")
