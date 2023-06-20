101119ffbba1f5fe072b68ba9367873cd2a7f6ce
Lucino772 <lucapalmi772@gmail.com>
2023-06-20 20:04:08 +0200

    refactor(mvc): added `send_file` method to view

--- app/interfaces/views/view.py
+++ app/interfaces/views/view.py
@@ -4,3 +4,6 @@ import typing as t
 class IView(t.Protocol):
     def render(self, template: str, **context):
         ...
+
+    def send_file(self, filename: str, source: str, type: str, fd: int = None):
+        ...
--- app/views/abstract.py
+++ app/views/abstract.py
@@ -1,7 +1,8 @@
+import os
 import typing as t
 from abc import ABC, abstractmethod
 
-from flask import render_template
+from flask import make_response, render_template
 
 from app.factory.controller import ControllerFactory
 from app.interfaces.controllers.controller import IController
@@ -19,6 +20,26 @@ class AbstractView(ABC, IView, t.Generic[Controller_T]):
     def __init__(self) -> None:
         self.controller: Controller_T = self.controller_factory.get_controller(
             self
+        )
+
+    def _stream_file_and_remove(filename: str, fd: int = None):
+        open_path = filename if fd is None else fd
+
+        with open(open_path, "rb", closefd=True) as fp:
+            yield from fp
+
+        os.remove(filename)
+
+    def send_file(self, filename: str, source: str, type: str, fd: int = None):
+        nbytes = os.stat(source).st_size
+        return make_response(
+            self._stream_file_and_remove(source, fd),
+            headers={
+                "Content-Disposition": f"inline; filename={filename}",
+                "Content-Length": nbytes,
+                "Content-Type": type,
+                "filename": filename,
+            },
         )
 
     def render(self, template: str, **context):
