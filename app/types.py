88d317681cdce9a7d559c6ca00350292bccffe0c
Lucino772 <lucapalmi772@gmail.com>
2023-06-21 23:30:48 +0200

    chore: removed unused file

--- app/types.py
+++
@@ -1,7 +0,0 @@
-import typing as t
-
-from app.interfaces.controllers.controller import IController
-from app.interfaces.views.view import IView
-
-Controller_T = t.TypeVar("Controller_T", bound=IController)
-ControllerFactoryMethod = t.Callable[[IView], Controller_T]
